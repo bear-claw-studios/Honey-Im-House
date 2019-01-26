35829ec204174aad4bda3832ea6d236139860f05
Gary DiCiano <garydiciano4@gmail.com>
2019-01-26 17:08:17 -0500

    Messed up revert
    Unfortunately I've mistakenly deleted one of Jarrett's models and I tried reverting to a previous commit to no avail. TrackBall script got reverted in the process so this is just a "back on track" commit.

---
+++ Honey-Im-House/Assets/Scripts/RagdollOnCommand.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollOnCommand : MonoBehaviour
+{
+    //Thanks buds https://answers.unity.com/questions/1287258/how-to-make-an-enemy-ragdoll-on-death.html
+
+    //This script is meant to keep the model from Ragdolling until the camera's first move
+    void Start()
+    {
+        SetKinematic(true);
+    }
+
+    void SetKinematic(bool newValue)
+    {
+        Rigidbody[] bodies = GetComponentsInChildren<Rigidbody>();
+        foreach (Rigidbody rb in bodies)
+            rb.isKinematic = newValue;
+    }
+
+    void GoRagdoll()
+    {
+        SetKinematic(false);
+        GetComponent<Animator>().enabled = false;
+    }
+}
