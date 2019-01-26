c78ce7a0908ad10752eeb35e6b246868041c46fe
BokononLives <jacob.heft@gmail.com>
2019-01-26 03:04:18 -0500

    proof of concept - change render material on trigger enter

---
+++ Honey-Im-House/Assets/Scripts/Guy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guy : MonoBehaviour
+{
+    public enum GuyState
+    {
+        Red,
+        Green
+    }
+
+    public GuyState State;
+    public Material RedMaterial;
+    public Material GreenMaterial;
+
+    void Start()
+    {
+    }
+    
+    void Update()
+    {
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Green")
+        {
+            State = GuyState.Green;
+            Renderer renderer = GetComponent<Renderer>();
+            if (renderer != null)
+            {
+                renderer.material = GreenMaterial;
+            }
+        }
+    }
+}
