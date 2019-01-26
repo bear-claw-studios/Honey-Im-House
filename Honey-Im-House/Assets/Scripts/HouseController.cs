185652f57ab5203832f455ee0f71878d49f77128
Gary DiCiano <garydiciano4@gmail.com>
2019-01-25 22:05:50 -0500

    First
    Got a simple cube to rotate based on player input

---
+++ Honey-Im-House/Assets/Scripts/HouseController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HouseController : MonoBehaviour
+{
+    //Rigidbody rb;
+
+    public float rotateSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        transform.Rotate(new Vector3(0.0f, 0.0f, h * rotateSpeed));
+        
+        //rb.MoveRotation(Quaternion)
+    }
+}
