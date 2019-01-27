5e587be4cdbf261a75ef4b089328196c31c69cd8
Gary DiCiano <garydiciano4@gmail.com>
2019-01-27 10:43:25 -0500

    I didn't get the job at Unity

---
+++ Honey-Im-House/Assets/Scripts/CallToChangeTrack.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CallToChangeTrack : MonoBehaviour
+{
+    public MusicController mc;
+
+    void Awake()
+    {
+        mc = FindObjectOfType<MusicController>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        mc.SkipTrack();
+    }
+}
---
+++ Honey-Im-House/Assets/Scripts/MusicController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+    public AudioSource currentTrack;
+    public AudioClip[] otherTracks;
+
+    void Start()
+    {
+        currentTrack = GetComponent<AudioSource>();
+    }
+
+    public void SkipTrack()
+    {
+        int index = Random.Range(0, otherTracks.Length - 1);
+        AudioClip nextTrack = otherTracks[index];
+        if (nextTrack == currentTrack)
+            SkipTrack();
+        currentTrack.clip = nextTrack;
+        currentTrack.Play();
+    }
+}
